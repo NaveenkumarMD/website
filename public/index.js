f3ffc0e22aef63806223c2657f4056ec9b0b817e
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2021-05-25 19:13:29 +0530

    done

---
+++ public/index.js
@@ -0,0 +1,19 @@
+function CopyToClipboard(containerid) {
+    if (document.selection) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById(containerid));
+      range.select().createTextRange();
+      document.execCommand("copy");
+    } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById(containerid));
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+      var alert=document.getElementById('copy-alert')
+      alert.style.display="block"
+      setTimeout(()=>{
+        alert.style.display="none"
+      },3000)
+      
+    }
+  }
